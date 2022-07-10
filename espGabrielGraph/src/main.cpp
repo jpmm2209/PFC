4a97ada9d3d7e45a4fcaffaada7b6c83a2e55557
jueta <jpmm.brasil@hotmail.com>
2022-07-10 19:33:29 -0300

    ccc

--- espGabrielGraph/src/main.cpp
+++ espGabrielGraph/src/main.cpp
@@ -8,40 +8,47 @@ extern ArrayXXd x_prd;
 
 void setup()
 {
+	
 	Serial.begin(9600);
-	Serial.println("Starting Program");
+	Serial.println("\n \n --------Starting Program!---------");
 	initValues();
 	delay(2000);
+
+	ArrayXXd Xi = X.transpose();
+
+	Serial.println("X = [ ");
+	for(int i = 0; i < X.rows() ; i++) {
+		for(int j = 0; j < X.cols(); j++){
+			Serial.print(X(i,j));
+			Serial.print(" ");
+		}
+		Serial.println();
+	}
+	Serial.println(" ]");
+
+	Serial.println("y.size() = ");
+	Serial.println(y.size());
+	Serial.println("x_prd.size() = ");
+	Serial.println(x_prd.size());
+
+	// Model
+	ListTest mdl = model(Xi, y, false);
+
+	Serial.println("Model Done!");
+	ArrayXXd Xtest = x_prd.transpose();
+
+	// Predict
+	ArrayXi prd = predict(mdl, Xtest);
+
+	Serial.println("Predict Done!");
+	Serial.println("prd.size() = ");
+	Serial.println(prd.size());
+
+
 }
 
 void loop()
 {
-
-	Serial.println("\n \n------- LOOP ---------");
-
-	ArrayXXd Xi = X.transpose();
-
-	Serial.println("X.size() = ");
-	Serial.println(X.size());
-
-	Serial.println("y.size() = ");
-	Serial.println(y.size());
-
-	Serial.println("x_prd.size() = ");
-	Serial.println(x_prd.size());
-
-	ListTest mdl = model(Xi, y, false);
-
-	Serial.println("Model Done!");
-
-	ArrayXXd Xtest = x_prd.transpose();
-
-	ArrayXi prd = predict(mdl, Xtest);
-
-	Serial.println("Predict Done!");
-
-	Serial.println("prd.size() = ");
-	Serial.println(prd.size());
-
+	Serial.print(" .");
 	delay(1000);
 }
