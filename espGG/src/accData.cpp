40f506815f964ae2337b8b48eb431b59d9cb43ff
jueta <jpmm.brasil@hotmail.com>
2022-07-13 16:20:30 -0300

    data read csv example

---
+++ espGG/src/accData.cpp
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+#include <espGG.h>
+#include <CSV_Parser.h>
+
+char *csv_str = "my_strings,my_numbers\n"
+                "hello,5\n"
+                "world,10\n";
--- espGG/src/main.cpp
+++ espGG/src/main.cpp
@@ -1,38 +1,79 @@
 #include <Arduino.h>
 #include <espGG.h>
+#include <CSV_Parser.h>
 
 using namespace Eigen;
 extern ArrayXXd X_test;
 extern ArrayXXd X_train;
 extern vector<int> y_train;
 extern vector<int> y_test;
+extern char *csv_str;
 
 void setup()
 {
-
 	Serial.begin(9600);
-	Serial.println("\n \n --------Starting Program!---------");
-	initValues();
-	delay(2000);
+	delay(5000);
 
-	printArrayXXd(X_train, "X_train");
-	printArrayXXd(X_test, "X_test");
-	printVector(y_train, "y_train");
+	CSV_Parser cp(csv_str, /*format*/ "sL");
 
-	// Model
-	ListTest mdl = model(X_train, y_train, false);
-	Serial.println("Model Done!");
+	Serial.println("Accessing values by column name:");
+	char **strings = (char **)cp["my_strings"];
+	int32_t *numbers = (int32_t *)cp["my_numbers"];
 
-	// Predict
-	ArrayXi prd = predict(mdl, X_test);
-	Serial.println("Predict Done!");
-	printArrayXi(prd, "Prediction");
+	for (int row = 0; row < cp.getRowsCount(); row++)
+	{
+		Serial.print(row, DEC);
+		Serial.print(". String = ");
+		Serial.println(strings[row]);
+		Serial.print(row, DEC);
+		Serial.print(". Number = ");
+		Serial.println(numbers[row], DEC);
+	}
+	Serial.println();
 
-	// Acuracy
-	float accuracy = testAccuracy(y_test, prd);
-	Serial.print("Acurácia: ");
-	Serial.print(accuracy);
-	Serial.println("%");
+	Serial.println("Accessing values by column number:");
+	strings = (char **)cp[0];
+	numbers = (int32_t *)cp[1];
+
+	for (int row = 0; row < cp.getRowsCount(); row++)
+	{
+		Serial.print(row, DEC);
+		Serial.print(". String = ");
+		Serial.println(strings[row]);
+		Serial.print(row, DEC);
+		Serial.print(". Number = ");
+		Serial.println(numbers[row], DEC);
+	}
+
+	/*
+	  If we wanted to just check if the csv was parsed properly we could use:
+	  cp.print();
+	  (assumming that "Serial.begin(baud_rate)" was previously called. Because "cp.print()" is using "Serial" object)
+	*/
+
+	// Serial.begin(9600);
+	// Serial.println("\n \n --------Starting Program!---------");
+	// initValues();
+	// delay(2000);
+
+	// printArrayXXd(X_train, "X_train");
+	// printArrayXXd(X_test, "X_test");
+	// printVector(y_train, "y_train");
+
+	// // Model
+	// ListTest mdl = model(X_train, y_train, false);
+	// Serial.println("Model Done!");
+
+	// // Predict
+	// ArrayXi prd = predict(mdl, X_test);
+	// Serial.println("Predict Done!");
+	// printArrayXi(prd, "Prediction");
+
+	// // Acuracy
+	// float accuracy = testAccuracy(y_test, prd);
+	// Serial.print("Acurácia: ");
+	// Serial.print(accuracy);
+	// Serial.println("%");
 }
 
 void loop()
@@ -58,5 +99,5 @@ float testAccuracy(vector<int> &y_hat, ArrayXi &y)
 		}
 	}
 
-	return (correct/sample)*100;
+	return (correct / sample) * 100;
 }
