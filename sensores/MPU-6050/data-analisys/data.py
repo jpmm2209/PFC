ed281dbcf3883033ec3f02f8b9977f0b6a4db183
jueta <jpmm.brasil@hotmail.com>
2022-09-15 15:42:14 -0300

    relatorio

--- sensores/MPU-6050/data-analisys/data.py
+++ sensores/MPU-6050/data-analisys/data.py
@@ -50,75 +50,75 @@ X_train['x_mean'] = pd.Series(x_list).apply(lambda x: x.mean())
 X_train['y_mean'] = pd.Series(y_list).apply(lambda x: x.mean())
 X_train['z_mean'] = pd.Series(z_list).apply(lambda x: x.mean())
 
-# std dev
-X_train['x_std'] = pd.Series(x_list).apply(lambda x: x.std())
-X_train['y_std'] = pd.Series(y_list).apply(lambda x: x.std())
-X_train['z_std'] = pd.Series(z_list).apply(lambda x: x.std())
+# # std dev
+# X_train['x_std'] = pd.Series(x_list).apply(lambda x: x.std())
+# X_train['y_std'] = pd.Series(y_list).apply(lambda x: x.std())
+# X_train['z_std'] = pd.Series(z_list).apply(lambda x: x.std())
 
-# avg absolute diff
-X_train['x_aad'] = pd.Series(x_list).apply(lambda x: np.mean(np.absolute(x - np.mean(x))))
-X_train['y_aad'] = pd.Series(y_list).apply(lambda x: np.mean(np.absolute(x - np.mean(x))))
-X_train['z_aad'] = pd.Series(z_list).apply(lambda x: np.mean(np.absolute(x - np.mean(x))))
+# # avg absolute diff
+# X_train['x_aad'] = pd.Series(x_list).apply(lambda x: np.mean(np.absolute(x - np.mean(x))))
+# X_train['y_aad'] = pd.Series(y_list).apply(lambda x: np.mean(np.absolute(x - np.mean(x))))
+# X_train['z_aad'] = pd.Series(z_list).apply(lambda x: np.mean(np.absolute(x - np.mean(x))))
 
-# min
-X_train['x_min'] = pd.Series(x_list).apply(lambda x: x.min())
-X_train['y_min'] = pd.Series(y_list).apply(lambda x: x.min())
-X_train['z_min'] = pd.Series(z_list).apply(lambda x: x.min())
+# # min
+# X_train['x_min'] = pd.Series(x_list).apply(lambda x: x.min())
+# X_train['y_min'] = pd.Series(y_list).apply(lambda x: x.min())
+# X_train['z_min'] = pd.Series(z_list).apply(lambda x: x.min())
 
-# max
-X_train['x_max'] = pd.Series(x_list).apply(lambda x: x.max())
-X_train['y_max'] = pd.Series(y_list).apply(lambda x: x.max())
-X_train['z_max'] = pd.Series(z_list).apply(lambda x: x.max())
+# # max
+# X_train['x_max'] = pd.Series(x_list).apply(lambda x: x.max())
+# X_train['y_max'] = pd.Series(y_list).apply(lambda x: x.max())
+# X_train['z_max'] = pd.Series(z_list).apply(lambda x: x.max())
 
-# max-min diff
-X_train['x_maxmin_diff'] = X_train['x_max'] - X_train['x_min']
-X_train['y_maxmin_diff'] = X_train['y_max'] - X_train['y_min']
-X_train['z_maxmin_diff'] = X_train['z_max'] - X_train['z_min']
+# # max-min diff
+# X_train['x_maxmin_diff'] = X_train['x_max'] - X_train['x_min']
+# X_train['y_maxmin_diff'] = X_train['y_max'] - X_train['y_min']
+# X_train['z_maxmin_diff'] = X_train['z_max'] - X_train['z_min']
 
 # median
 X_train['x_median'] = pd.Series(x_list).apply(lambda x: np.median(x))
 X_train['y_median'] = pd.Series(y_list).apply(lambda x: np.median(x))
 X_train['z_median'] = pd.Series(z_list).apply(lambda x: np.median(x))
 
-# median abs dev 
-X_train['x_mad'] = pd.Series(x_list).apply(lambda x: np.median(np.absolute(x - np.median(x))))
-X_train['y_mad'] = pd.Series(y_list).apply(lambda x: np.median(np.absolute(x - np.median(x))))
-X_train['z_mad'] = pd.Series(z_list).apply(lambda x: np.median(np.absolute(x - np.median(x))))
+# # median abs dev 
+# X_train['x_mad'] = pd.Series(x_list).apply(lambda x: np.median(np.absolute(x - np.median(x))))
+# X_train['y_mad'] = pd.Series(y_list).apply(lambda x: np.median(np.absolute(x - np.median(x))))
+# X_train['z_mad'] = pd.Series(z_list).apply(lambda x: np.median(np.absolute(x - np.median(x))))
 
-# interquartile range
-X_train['x_IQR'] = pd.Series(x_list).apply(lambda x: np.percentile(x, 75) - np.percentile(x, 25))
-X_train['y_IQR'] = pd.Series(y_list).apply(lambda x: np.percentile(x, 75) - np.percentile(x, 25))
-X_train['z_IQR'] = pd.Series(z_list).apply(lambda x: np.percentile(x, 75) - np.percentile(x, 25))
+# # interquartile range
+# X_train['x_IQR'] = pd.Series(x_list).apply(lambda x: np.percentile(x, 75) - np.percentile(x, 25))
+# X_train['y_IQR'] = pd.Series(y_list).apply(lambda x: np.percentile(x, 75) - np.percentile(x, 25))
+# X_train['z_IQR'] = pd.Series(z_list).apply(lambda x: np.percentile(x, 75) - np.percentile(x, 25))
 
-# negtive count
-X_train['x_neg_count'] = pd.Series(x_list).apply(lambda x: np.sum(x < 0))
-X_train['y_neg_count'] = pd.Series(y_list).apply(lambda x: np.sum(x < 0))
-X_train['z_neg_count'] = pd.Series(z_list).apply(lambda x: np.sum(x < 0))
+# # negtive count
+# X_train['x_neg_count'] = pd.Series(x_list).apply(lambda x: np.sum(x < 0))
+# X_train['y_neg_count'] = pd.Series(y_list).apply(lambda x: np.sum(x < 0))
+# X_train['z_neg_count'] = pd.Series(z_list).apply(lambda x: np.sum(x < 0))
 
-# positive count
-X_train['x_pos_count'] = pd.Series(x_list).apply(lambda x: np.sum(x > 0))
-X_train['y_pos_count'] = pd.Series(y_list).apply(lambda x: np.sum(x > 0))
-X_train['z_pos_count'] = pd.Series(z_list).apply(lambda x: np.sum(x > 0))
+# # positive count
+# X_train['x_pos_count'] = pd.Series(x_list).apply(lambda x: np.sum(x > 0))
+# X_train['y_pos_count'] = pd.Series(y_list).apply(lambda x: np.sum(x > 0))
+# X_train['z_pos_count'] = pd.Series(z_list).apply(lambda x: np.sum(x > 0))
 
-# values above mean
-X_train['x_above_mean'] = pd.Series(x_list).apply(lambda x: np.sum(x > x.mean()))
-X_train['y_above_mean'] = pd.Series(y_list).apply(lambda x: np.sum(x > x.mean()))
-X_train['z_above_mean'] = pd.Series(z_list).apply(lambda x: np.sum(x > x.mean()))
+# # values above mean
+# X_train['x_above_mean'] = pd.Series(x_list).apply(lambda x: np.sum(x > x.mean()))
+# X_train['y_above_mean'] = pd.Series(y_list).apply(lambda x: np.sum(x > x.mean()))
+# X_train['z_above_mean'] = pd.Series(z_list).apply(lambda x: np.sum(x > x.mean()))
 
 # number of peaks
 X_train['x_peak_count'] = pd.Series(x_list).apply(lambda x: len(find_peaks(x)[0]))
 X_train['y_peak_count'] = pd.Series(y_list).apply(lambda x: len(find_peaks(x)[0]))
 X_train['z_peak_count'] = pd.Series(z_list).apply(lambda x: len(find_peaks(x)[0]))
 
-# skewness
-X_train['x_skewness'] = pd.Series(x_list).apply(lambda x: stats.skew(x))
-X_train['y_skewness'] = pd.Series(y_list).apply(lambda x: stats.skew(x))
-X_train['z_skewness'] = pd.Series(z_list).apply(lambda x: stats.skew(x))
+# # skewness
+# X_train['x_skewness'] = pd.Series(x_list).apply(lambda x: stats.skew(x))
+# X_train['y_skewness'] = pd.Series(y_list).apply(lambda x: stats.skew(x))
+# X_train['z_skewness'] = pd.Series(z_list).apply(lambda x: stats.skew(x))
 
-# kurtosis
-X_train['x_kurtosis'] = pd.Series(x_list).apply(lambda x: stats.kurtosis(x))
-X_train['y_kurtosis'] = pd.Series(y_list).apply(lambda x: stats.kurtosis(x))
-X_train['z_kurtosis'] = pd.Series(z_list).apply(lambda x: stats.kurtosis(x))
+# # kurtosis
+# X_train['x_kurtosis'] = pd.Series(x_list).apply(lambda x: stats.kurtosis(x))
+# X_train['y_kurtosis'] = pd.Series(y_list).apply(lambda x: stats.kurtosis(x))
+# X_train['z_kurtosis'] = pd.Series(z_list).apply(lambda x: stats.kurtosis(x))
 
 # energy
 X_train['x_energy'] = pd.Series(x_list).apply(lambda x: np.sum(x**2)/100)
@@ -136,7 +136,6 @@ X_train['sma'] =    pd.Series(x_list).apply(lambda x: np.sum(abs(x)/100)) + pd.S
 # plots
 ########
 
-plt.plot(X_train)
-# plt.plot(X_train['sma'])
-
+X_train.plot(kind='line')
+# plt.plot(X_train)
 plt.show()
